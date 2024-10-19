f51f64a6481d11b0739898916847e7ab9d50efa5
Abolfazl Erfani <44287598+090ebier@users.noreply.github.com>
2024-10-19 16:40:44 +0330

    Update ovs_management.sh

--- Module/ovs_management.sh
+++ Module/ovs_management.sh
@@ -1066,7 +1066,7 @@ function ovs_management(){
                 --menu "\nChoose an action:" "$dialog_height" "$dialog_width" 4 \
                 1 "\Zb\Z2Backup Configuration\Zn" \
                 2 "\Zb\Z2Restore Configuration\Zn" \
-                3 "\Zb\Z2Delete Backup Configuration\Zn"
+                3 "\Zb\Z2Delete Backup Configuration\Zn" \
                 4 "\Zb\Z1Return to Previous Menu\Zn" \ 3>&1 1>&2 2>&3 )
             # بررسی لغو عملیات
             if [ $? -ne 0 ]; then continue; fi
